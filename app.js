ce3c246037234d3f26ff0f5f0a605a0cbaa3a15c
shobhit shukla <engshobhitshukla07@gmail.com>
2026-01-20 03:07:21 +0530

    fix user route prefix

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const userRoutes = require('./routes/user.routes');
-const dotenv =require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
+
+const cookieParser = require("cookie-parser");
 const connectToDB = require("./config/db");
 connectToDB();
-const cookieParser =require('cookie-parser');
 
 const app = express();
-const indexRouter = require('./routes/index.routes')
 
-app.set('view engine', 'ejs');
-app.use(cookieParser())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const indexRouter = require("./routes/index.routes");
+const userRoutes = require("./routes/user.routes");
 const uploadRoutes = require("./routes/upload.routes");
 const fileRoutes = require("./routes/file.routes");
 
+app.set("view engine", "ejs");
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/',indexRouter)
+app.use("/", indexRouter);
 app.use("/", uploadRoutes);
 app.use("/", fileRoutes);
 
+
+app.use("/user", userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
